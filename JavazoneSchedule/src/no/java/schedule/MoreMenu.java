49b4f9e9d5d72509d1a06010bc3d834a12249a10
Erlend Birkenes <erlend@laptop2.birkenes.net>
2010-03-23 18:04:31 +0100

    Lots of small changes after workshop. Refactored packages.

---
+++ JavazoneSchedule/src/no/java/schedule/MoreMenu.java
@@ -0,0 +1,135 @@
+package no.java.schedule;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import no.java.schedule.util.AppUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: erlend
+ * Date: Mar 19, 2010
+ * Time: 10:37:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MoreMenu extends ListActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getMenuItems());
+        setListAdapter(adapter);
+
+    }
+
+
+    public List<String> getMenuItems() {
+
+        List<String> l = new ArrayList();
+
+        l.add("Twitter");
+        l.add("Area Map");
+        l.add("Conference Map");
+        l.add("FAQ");
+        l.add("Sponsors");
+        l.add("About");
+
+
+        return l;
+    }
+
+
+
+     @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);    //To change body of overridden methods use File | Settings | File Templates.
+
+    switch (position) {
+        case 0:
+            openTwitterView();
+            break;
+        case 1:
+            openGoogleMapView();
+            break;
+        case 2:
+            AppUtil.showLevel(this, 1);
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            
+            break;
+        case 5:
+            showDialog(R.id.dialog_about);
+            break;
+
+    }
+
+
+    }
+
+    protected void openTwitterView() {
+        Intent intent = new Intent(this, TwitterActivity.class);
+        startActivity(intent);
+    }
+
+     protected void openGoogleMapView() {
+        Intent intent = new Intent(this, GoogleMapActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+//            case R.id.dialog_load:
+//                return buildLoadingDialog();
+//            case R.id.dialog_tracks:
+//                return buildTracksDialog();
+            case R.id.dialog_about:
+                return buildAboutDialog();
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Build about dialog.
+     */
+    private Dialog buildAboutDialog() {
+
+        String versionName = null;
+        try {
+            PackageInfo pi = getPackageManager().getPackageInfo(getPackageName(), 0);
+            versionName = pi.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+
+        View view = getLayoutInflater().inflate(R.layout.about, null, false);
+
+        TextView version = (TextView)view.findViewById(R.id.version);
+        version.setText(getString(R.string.about_version, versionName));
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.app_name));
+        builder.setIcon(android.R.drawable.ic_dialog_info);
+        builder.setView(view);
+        builder.setPositiveButton(getString(android.R.string.ok), null);
+        builder.setCancelable(true);
+
+        return builder.create();
+    }
+
+}
---
+++ JavazoneSchedule/src/no/java/schedule/TwitterActivity.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2009 Virgil Dobjanschi, Jeff Sharkey, Filip Maelbrancke
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package no.java.schedule;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+
+/**
+ * An activity which displays an expandable list
+ */
+public class TwitterActivity extends Activity {
+    private static final String TAG = "TwitterActivity";
+
+    WebView webview;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webview = new WebView(this);
+
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //getWindow().requestFeature(Window.FEATURE_PROGRESS);
+
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setKeepScreenOn(true);
+
+        setContentView(webview);
+
+        webview.loadUrl("file:///android_asset/twitter.html");
+        //TODO autodetect screensize for the twitter widget.
+    }
+}
