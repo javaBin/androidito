03a5779e3cc4dd529d104bb0d4cb74c82f6f8760
Øyvind Løkling <oyvind@lokling.com>
2010-05-22 10:36:58 +0200

    Added build files

---
+++ JavazoneSchedule/tests/src/no/java/schedule/provider/parsers/SessionsParserTest.java
@@ -0,0 +1,19 @@
+package no.java.schedule.provider.parsers;
+
+import android.test.AndroidTestCase;
+
+public class SessionsParserTest extends AndroidTestCase {
+    private SessionsParser sessionParser;
+
+
+    public void setUp() throws Exception {
+        //sessionParser = new SessionsParser();
+
+    }
+
+    
+    public void testParseSessions() throws Exception {
+      //TODO
+
+    }
+}
